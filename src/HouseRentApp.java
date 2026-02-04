fd07fc62a16e1732b4c69762183bc8332f1f71a7
Liu Kai <liukai306@163.com>
2026-02-04 17:19:22 +0800

    add HouseRentApp.java

---
+++ src/HouseRentApp.java
@@ -0,0 +1,9 @@
+public class HouseRentApp {
+    public static void main(String[] args) {
+        System.out.println("===========================房屋出租系统启动=======================");
+
+
+
+        System.out.println("===========================房屋出租系统退出=======================");
+    }
+}
